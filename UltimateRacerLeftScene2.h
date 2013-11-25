a8acf00b4191680a1a1f5d9fe058a48deaae6af3
Sagar Patel <sjpatel6@g.ucla.edu>
2013-11-24 20:56:23 -0800

    new track

---
+++ UltimateRacerLeftScene2.h
@@ -0,0 +1,13 @@
+//
+//  UltimateRacerLeftScene2.h
+//  UltimateRacer
+//
+//  Created by Sagar Patel on 24/11/13.
+//  Copyright (c) 2013 Samir Choudhary. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface UltimateRacerLeftScene2 : SKScene
+
+@end

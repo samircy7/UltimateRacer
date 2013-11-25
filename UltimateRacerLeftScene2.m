53fd568859468464a8f42792aa3d8b95003a85fa
Sagar Patel <sjpatel6@g.ucla.edu>
2013-11-24 21:53:36 -0800

    new track

--- UltimateRacerLeftScene2.m
+++ UltimateRacerLeftScene2.m
@@ -28,19 +28,28 @@
         CGRect selfSize = self.frame;
         selfSize.origin.x = selfSize.size.width/2 - 350;
         selfSize.origin.y = selfSize.size.height/2 - 112;
-        selfSize.size.height -= 700;
+        selfSize.size.height -= 800;
         selfSize.size.width -= 75;
         
         track1 = [SKShapeNode node];
         UIBezierPath *path = [UIBezierPath bezierPath];
         [path moveToPoint:selfSize.origin];
-        [path addLineToPoint:CGPointMake(selfSize.origin.x + selfSize.size.width/2, selfSize.origin.y)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + selfSize.size.width/3, selfSize.origin.y)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + 2 * selfSize.size.width/3, selfSize.origin.y + selfSize.size.height)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + selfSize.size.width, selfSize.origin.y + selfSize.size.height)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + selfSize.size.width, selfSize.origin.y)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + 2 * selfSize.size.width/3, selfSize.origin.y)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x + selfSize.size.width/3, selfSize.origin.y + selfSize.size.height)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x, selfSize.origin.y + selfSize.size.height)];
+        [path addLineToPoint:CGPointMake(selfSize.origin.x, selfSize.origin.y)];
+        path.lineCapStyle = kCGLineCapRound;
+        path.lineJoinStyle = kCGLineJoinRound;
         track1.path = [path CGPath];
         
         track1.fillColor = [UIColor clearColor];
         track1.strokeColor = [UIColor colorWithRed:0 green:0 blue:0.9 alpha:1];
         track1.glowWidth = 7;
-        track1.lineWidth = 23;
+        track1.lineWidth = 5;
         
         [self addChild:track1];
     }
